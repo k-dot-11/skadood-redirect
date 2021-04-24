0d8d47bf3c661ce01494a53c7950fc42ad0a66c2
k-dot-11 <karnatheveer@gmail.com>
2021-04-24 11:59:33 +0530

    stage2

--- pages/redirect.js
+++ pages/redirect.js
@@ -1,10 +1,39 @@
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 const Post = () => {
-  const router = useRouter()
-  const { code } = router.query
+	const router = useRouter();
+	const { code } = router.query;
 
-  return <p>Code: {code}</p>
-}
+	const [ userEmail, setEmail ] = useState('');
 
-export default Post
+	const setToken = () => {
+		fetch('http://localhost:8000/set-token', {
+			method: 'POST',
+			mode: 'cors',
+			cache: 'no-cache',
+			credentials: 'same-origin',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			redirect: 'follow',
+			referrerPolicy: 'no-referrer',
+			body: JSON.stringify({ email: userEmail, authcode: code })
+		});
+	};
+
+	return (
+		<div>
+			<p>Code: {code}</p>
+			<input
+				onChange={(input) => {
+					setEmail(input.target.value.toString());
+				}}
+				value={userEmail}
+			/>
+			<button onClick={setToken}>GO</button>
+		</div>
+	);
+};
+
+export default Post;
